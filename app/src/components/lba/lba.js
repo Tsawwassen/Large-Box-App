aec1b4d420f5882bb9f2cb67ca50dbd5d977081b
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 11:39:26 -0700

    Add LBAInput component

--- app/src/components/lba/lba.js
+++ app/src/components/lba/lba.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
+import LBAInput from './lba_input.js';
+
+
 
 class LargeBoxApp extends Component {
 
 	constructor(props){
+
 		super(props);
+
+		//State values
+		this.INPUT_STATE = "input";
+		this.PRINT_STATE = "print";
+
 		this.state = {
+			view: this.INPUT_STATE,
 			value: ''
 		};
 
+		//LBA Input Binds
 		this.handleCreateButton = this.handleCreateButton.bind(this);
-
 		this.handleInputChange = this.handleInputChange.bind(this);
+
 	}
 
 	handleCreateButton(event){
   		console.log(this.state.value);
+
+  		this.setState({view: this.PRINT_STATE});
+
   		event.preventDefault();
   	}
 
@@ -23,18 +37,21 @@ class LargeBoxApp extends Component {
   	}
 
 
+
 	render () {
+		console.log( this.state.view === this.INPUT_STATE );
+		console.log(this.state.view);
+		console.log(this.INPUT_STATE);
 		return (
 			<div>
-	 			<h1>LargeBoxApp2</h1>
-	 			<form onSubmit={this.handleCreateButton}>
-					<label>
-						Input:
-						<textarea value={this.state.value} onChange={this.handleInputChange} />
-					</label>
-					<br />
-				  <input type="submit" value="Create" />
-				</form>
+	 			<h1>LargeBoxApp</h1>
+	 			{this.state.view === this.INPUT_STATE &&
+					<LBAInput value={this.state.value} onChange={this.handleInputChange} onSubmit={this.handleCreateButton} />
+				}
+				{this.state.view === this.PRINT_STATE &&
+					<h1>print</h1>
+				}
+
 	 		</div>
 		);
   	}
