b035e1d01008a80485f4b4083f6ef84883960db7
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 14:19:55 -0700

    Add LBA Output component to React app

---
+++ app/src/components/lba/lba_output.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LBAOutput extends Component {
+	render () {
+		return (
+			<div>
+				<h1>print component</h1>
+			</div>
+		);
+  	}
+}
+
+export default LBAOutput;
