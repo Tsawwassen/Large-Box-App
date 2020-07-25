e1a67e2e296d85f3a3a53566dfe32aaa14f7c77a
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 12:16:39 -0700

    Add input to hash parsing

--- app/src/components/lba/lba.js
+++ app/src/components/lba/lba.js
@@ -13,9 +13,17 @@ class LargeBoxApp extends Component {
 		this.INPUT_STATE = "input";
 		this.PRINT_STATE = "print";
 
+		//Input Index keys
+		this.REQ_ID_INDEX = 0;
+		this.NAME_INDEX = 1;
+		this.LENGTH_INDEX = 2;
+		this.WIDTH_INDEX = 3;
+		this.HEIGHT_INDEX = 4;
+
 		this.state = {
 			view: this.INPUT_STATE,
-			value: ''
+			value: '',
+			labels: []
 		};
 
 		//LBA Input Binds
@@ -25,8 +33,32 @@ class LargeBoxApp extends Component {
 	}
 
 	handleCreateButton(event){
-  		console.log(this.state.value);
 
+  		//Parse value to get label information
+
+  		let input = this.state.value.split(/\r?\n/); //Each line in value is a lable request
+
+  		if(input[input.length-1].length === 0){ //Check the last elementis blank, remove it from the array if it is
+  			input.pop(); 
+  		}
+  		
+  		let output = []
+  		for(let i = 0 ; i < input.length ; i++){ //Parse each line for label information
+  			input[i] = input[i].split(/\t/);
+  			output[i] = {}; 
+  			output[i].id = input[i][this.REQ_ID_INDEX];
+  			output[i].name = input[i][this.NAME_INDEX];
+  			output[i].l = input[i][this.LENGTH_INDEX];
+  			output[i].w = input[i][this.WIDTH_INDEX];
+  			output[i].h =  input[i][this.HEIGHT_INDEX];
+  		}
+
+  		//console.log(output);
+  		//Once the array is populated, check for label requests that are too big.
+  		////Swap values as needed.
+
+  		
+  		this.setState({labels: output});
   		this.setState({view: this.PRINT_STATE});
 
   		event.preventDefault();
@@ -39,9 +71,6 @@ class LargeBoxApp extends Component {
 
 
 	render () {
-		console.log( this.state.view === this.INPUT_STATE );
-		console.log(this.state.view);
-		console.log(this.INPUT_STATE);
 		return (
 			<div>
 	 			<h1>LargeBoxApp</h1>
