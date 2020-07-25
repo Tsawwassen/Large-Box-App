ad7478ac5236b505d17b6ba7f58fbf024753cb5f
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 10:04:56 -0700

    Add Navbar, routes, and boostrap css to app component

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Components import
+import Navigation from '../nav/nav.js';
+
+//CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<Router>
+	  	<div className="App">
+			<Navigation />
+			<Switch>
+			  	<Route path="/">
+					<h1> hello</h1>
+			  	</Route>
+			</Switch>
+		</div>
+	</Router>
   );
 }
 
