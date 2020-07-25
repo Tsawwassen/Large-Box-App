b27ccc95939e9b497f67eaecfd8b219c2fea5bdd
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 14:48:50 -0700

    Display content of labels list in lba_output

--- app/src/components/lba/lba_output.js
+++ app/src/components/lba/lba_output.js
@@ -6,6 +6,11 @@ class LBAOutput extends Component {
 		return (
 			<div>
 				<h1>print component</h1>
+				
+				{this.props.labels.map((value, index) => {
+        			return <p key={index}>{value.id}</p>
+      			})}
+      			
 				<button onClick={this.props.onClick}>Back</button>
 			</div>
 		);
