1106b3045de9ff4074e4355783bcd1b1260c4019
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-25 10:02:30 -0700

    Add Navbar component to project

---
+++ app/src/components/nav/nav.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+
+class Navigation extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Warehouse Label App</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Large Box App</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
