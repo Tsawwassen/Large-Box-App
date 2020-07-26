d1e9f4cbfb32fb3a772613b1d45d594d9d97bfcc
Mitchell <mitchell.rian.smith@gmail.com>
2020-07-26 11:29:24 -0700

    Add more rows to LBA input text area

--- app/src/components/lba/lba_input.js
+++ app/src/components/lba/lba_input.js
@@ -8,7 +8,7 @@ class LBAInput extends Component {
 					<div class="form-group row">
 						<label for="lba_input" className="col-sm-2 col-form-label"> Input: </label>
 						<div class="col-sm-10">
-							<textarea className="form-control" id="lba_input" value={this.props.value} onChange={this.props.onChange} />
+							<textarea className="form-control" id="lba_input" value={this.props.value} onChange={this.props.onChange} rows="15" />
 						</div>
 					</div>
 					<br />
