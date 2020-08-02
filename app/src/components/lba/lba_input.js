126cb65d71a897bb9fa324ba4d215135f84a4b87
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-02 09:18:20 -0700

    Update CSS to SCSS

--- app/src/components/lba/lba_input.js
+++ app/src/components/lba/lba_input.js
@@ -5,11 +5,12 @@ class LBAInput extends Component {
 		return (
 			<div>
 				<form onSubmit={this.props.onSubmit}>
-					<div class="form-group row">
-						<label for="lba_input" className="col-sm-2 col-form-label"> Input: </label>
-						<div class="col-sm-10">
-							<textarea className="form-control" id="lba_input" value={this.props.value} onChange={this.props.onChange} rows="15" />
-						</div>
+					<div className="form-group row">
+						<label className="col-sm-10 col-form-label"> Input: 
+					
+								<textarea className="form-control" id="lba_input" value={this.props.value} onChange={this.props.onChange} rows="15" />
+						
+						</label>
 					</div>
 					<br />
 					<button type="submit" className="btn btn-primary">Submit</button>
